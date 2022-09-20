0afadcfe45f38ba4555d695e795161876e9aba13
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2022-09-20 17:47:24 +0200

    Addrd prosses - Add Product

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
+import { Create_Product } from 'src/app/contracts/create_product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -17,9 +17,9 @@ export class ProductsComponent extends BaseComponent implements OnInit {
 
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallAtom);
-    this.httpClientService.get<Product[]>({
-      controller: "products"
-    }).subscribe(data => console.log(data));
+    //this.httpClientService.get<Create_Product[]>({
+    //  controller: "products"
+    //}).subscribe(data => console.log(data));
     /*
         this.httpClientService.post({
           controller:"products"
