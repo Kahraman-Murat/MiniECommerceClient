df8a1e070663c5d97b11795e561f98dee895a973
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2022-10-31 00:40:18 +0100

    Architecturally added SignalR Infrastructure Integration

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { HubUrls } from '../../../constants/hub-urls';
+import { ReceiveFunctions } from '../../../constants/receive-functions';
+import { SignalRService } from '../../../services/common/signalr.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,13 +13,26 @@ import { AlertifyService, MessageType, Position } from 'src/app/services/admin/a
 })
 export class DashboardComponent extends BaseComponent implements OnInit {
 
-  constructor(private alertify: AlertifyService, spinner: NgxSpinnerService) {
+  constructor(private alertify: AlertifyService, spinner: NgxSpinnerService, private signalRService: SignalRService) {
     super(spinner)
+    signalRService.start(HubUrls.ProductHub)
   }
 
   ngOnInit(): void {
     //this.showSpinner(SpinnerType.BallAtom);
+
+    this.signalRService.on(ReceiveFunctions.ProductAddedMessageReceiveFunction, message => {
+      //alert(message);
+      this.alertify.message(message, {
+        messageType: MessageType.Notify,
+        position: Position.TopLeft,
+        delay: 5,
+        dismissOthers: false
+      })
+    });
   }
+
+
   m() {
 
     this.alertify.message("Merhaba", {
