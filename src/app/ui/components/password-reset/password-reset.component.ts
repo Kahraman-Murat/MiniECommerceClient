43ecddbc6353568e3a0057fa0d9225cd2564ea88
muratkahraman.de@gmail.com <muratkahraman.de@gmail.com>
2022-11-16 16:25:57 +0100

    Added EMail Service Added Password Reset and User Password Update

---
+++ src/app/ui/components/password-reset/password-reset.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from '../../../base/base.component';
+import { AlertifyService, MessageType, Position } from '../../../services/admin/alertify.service';
+import { UserAuthService } from '../../../services/common/models/user-auth.service';
+
+@Component({
+  selector: 'app-password-reset',
+  templateUrl: './password-reset.component.html',
+  styleUrls: ['./password-reset.component.scss']
+})
+export class PasswordResetComponent extends BaseComponent {
+
+  constructor(spinner: NgxSpinnerService,
+    private userAuthService: UserAuthService,
+    private alertifyService: AlertifyService) {
+    super(spinner)
+  }
+
+  passwordReset(email: string) {
+    this.showSpinner(SpinnerType.BallAtom);
+    this.userAuthService.passwordReset(email, () => {
+      this.hideSpinner(SpinnerType.BallAtom)
+      this.alertifyService.message("Email Basari ile gönderilmistir.", {
+        messageType: MessageType.Notify,
+        position: Position.TopLeft
+      })
+    })
+
+  }
+
+}
